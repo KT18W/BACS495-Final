6e7f36dae672e11264910108cfadc51a06a561ac
Katie <galaxyventurer2000@gmail.com>
2022-03-02 15:24:47 -0700

    try to add modals

--- final/src/App.js
+++ final/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
+import registrationModal from './registrationModal';
+import loginModal from './loginModal';
 import treasure from './images/treasure.jpg';
 
 
@@ -11,6 +13,10 @@ function App() {
       <body>
         body
         <img className="Pic" src={treasure} alt="Treasure Chest" />
+        <button type="button" class="btn btn-info">Login</button>
+        <loginModal />
+        <button type="button" class="btn btn-info">Register</button>
+        <registrationModal />
       </body>
       <Footer />
     </div>
---
+++ final/src/registrationModal.js
@@ -0,0 +1,20 @@
+import React from 'react'
+const registrationModal = props => {
+    return(
+        <div className="registrationModal">
+            <div className="registrationModal-content">
+                <div className="registrationModal-header">
+                    <h5>header</h5>
+                </div>
+                <div className="registrationModal-body">
+                    <h5>body</h5>
+                </div>
+                <div className="registrationModal-footer">
+                    <h5>footer</h5>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default registrationModal
