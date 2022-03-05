6cf241cfbdfd8f15fad5277378d5df587a89cc15
Katie <galaxyventurer2000@gmail.com>
2022-03-04 20:26:05 -0700

    basically go back 2 commits

---
+++ generator/routes/users.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+var students = [
+  {"id":1, "name":"Joel"},
+  {"id":2, "name":"Katie"}
+]
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(students);
+});
+
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+  for(let student of students){
+    if(student.id == id){
+      res.json(student);
+    }
+  }
+  res.send("Cannot find student");
+});
+
+router.put('/:id', function(req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+  for(let student of students){
+    if(student.id == id){
+      res.json(student);
+    }
+  }
+  res.send("Cannot find student");
+});
+
+router.post('/:id', function(req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+  for(let student of students){
+    if(student.id == id){
+      res.json(student);
+    }
+  }
+  res.send("Cannot find  student");
+}); 
+
+router.delete('/:id', function(req, res, next) {
+  var id = req.params.id;
+  console.log(id);
+  for(let student of students){
+    if(student.id == id){
+      res.json(student);
+    }
+  }
+  res.send("Cannot find student");
+});
+
+module.exports = router;
