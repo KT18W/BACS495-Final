1561dadf674af32f984b77194d67feff7f67fa50
Katie <galaxyventurer2000@gmail.com>
2022-03-04 23:15:05 -0700

    login/logout buttons change places

--- final/src/App.js
+++ final/src/App.js
@@ -1,20 +1,35 @@
+import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import treasure from './images/treasure.jpg';
-import { useForm } from "react-hook-form";
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={apiResponse: ""};
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <body>
-        body
-        <img className="Pic" src={treasure} alt="Treasure Chest" />
-      </body>
-      <Footer />
-    </div>
-  );
+  callAPI(){
+    fetch("http://localhost:9000/")
+    .then(res => res.text())
+    .then(res => this.setState({apiResponse: res}))
+  }
+
+  componentWillMount(){
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <body>
+          <img className="Pic" src={treasure} alt="Treasure Chest" />
+        </body>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- final/src/Header.js
+++ final/src/Header.js
@@ -1,35 +1,48 @@
-import React from 'react'
-import './Header.css';
-import { useForm } from "react-hook-form";
+import React, {useState} from 'react'; 
 import lines from './images/lines.jpg';
+import './Header.css';
 
-  const Header = () => {  
-    const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => {
-      console.log(data)
+function Header() {
+
+  const [value, setValue] = useState("");
+
+  const submit = e => {
+    e.preventDefault();
+    if (!value){
+      setValue(true);
     }
-    return (
-      <div className="Header">
-        <button className="linesButton">
-            <img className="Lines" src={lines} alt="Menu" />
-        </button>
-        <h1 className="Title">
-            The Answers to University
-        </h1>
-        <form onSubmit={handleSubmit(onSubmit)}>        
-            <label>
-              Username:
-              <input type="text" name="username" ref={register('username', {required: true})}/>       
-            </label>
-            <label>
-              Password:
-              <input type="password" name="password" ref={register('password', {required: true})} />       
-            </label>
-            <input type="submit" value="login"/>
-          </form>
-      </div>
-    )
-  }
+    if (value){
+      setValue(false);
+    }
+  };
 
+  return (
+
+    <div className="Header">
+      <button className="linesButton">
+          <img className="Lines" src={lines} alt="Menu" />
+      </button>
+      <h1 className="Title">
+          The Answers to University
+      </h1>
+          <div className="done">
+            {!value ? (<form onSubmit={submit}>        
+          <label>
+            Username:
+            <input type="text" name="username" />       
+          </label>
+          <label>
+            Password:
+            <input type="password" name="password" />       
+          </label>
+          <input type="submit" value="login"/>
+        </form>
+        ) : <form onSubmit={submit}>        
+        <input type="submit" value="logout"/>
+      </form>}
+          </div>
+      </div>
+  );
+}
 
 export default Header;
